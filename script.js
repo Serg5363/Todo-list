74e82f591e8f9d0c27da71dd009c0c30d1acc471
Serg5363 <126021764+Serg5363@users.noreply.github.com>
2023-02-26 22:17:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,18 @@ const todoList = document.getElementById('todo-list');
 const completeAllButton = document.getElementById('complete-all');
 const deleteCompletedButton = document.getElementById('delete-completed');
 
-// Загрузите существующие задачи из локального хранилища
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-// Функция для сохранения задач в локальном хранилище
 function saveTodos() {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-// Функция для создания нового элемента todo
 function createTodoItem(todoText, isCompleted = false) {
     const li = document.createElement('li');
     const checkbox = document.createElement('input');
     const label = document.createElement('label');
     const deleteButton = document.createElement('button');
 
-    // Установите checkbox
     checkbox.type = 'checkbox';
     checkbox.checked = isCompleted;
     checkbox.addEventListener('change', () => {
@@ -28,7 +24,6 @@ function createTodoItem(todoText, isCompleted = false) {
         saveTodos();
     });
 
-    // Настройте label
     label.innerText = todoText;
     label.contentEditable = true;
     label.addEventListener('input', () => {
@@ -36,7 +31,6 @@ function createTodoItem(todoText, isCompleted = false) {
         saveTodos();
     });
 
-    // Настройте кнопку удаления
     deleteButton.innerText = 'Удалить';
     deleteButton.addEventListener('click', () => {
         const index = todos.indexOf(todos);
@@ -47,12 +41,10 @@ function createTodoItem(todoText, isCompleted = false) {
         }
     });
 
-    // Добавьте элементы в li
     li.appendChild(checkbox);
     li.appendChild(label);
     li.appendChild(deleteButton);
 
-    // Добавьте завершенный класс, если это необходимо
     if (isCompleted) {
         li.classList.add('completed');
     }
@@ -60,19 +52,15 @@ function createTodoItem(todoText, isCompleted = false) {
     return li;
 }
 
-// Функция для отображения задач на странице
 function renderTodos() {
-    // Удалите существующие элементы todo
     todoList.innerHTML = '';
 
-    // Создайте новый элемент todo для каждого todo в массиве
     for (const todo of todos) {
         const li = createTodoItem(todo.text, todo.isCompleted);
         todoList.appendChild(li);
     }
 }
 
-// Прослушиватель событий для добавления нового todo
 todoForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const todoText = todoInput.value.trim();
@@ -86,7 +74,6 @@ todoForm.addEventListener('submit', (event) => {
     }
 });
 
-// Прослушиватель событий для завершения всех задач
 completeAllButton.addEventListener('click', () => {
     for (const todo of todos) {
         todo.isCompleted = true;
@@ -95,12 +82,10 @@ completeAllButton.addEventListener('click', () => {
     saveTodos();
 });
 
-// Прослушиватель событий для удаления завершенных задач
 deleteCompletedButton.addEventListener('click', () => {
 todos = todos.filter((todo) => !todo.isCompleted);
 renderTodos();
 saveTodos();
 });
 
-// Визуализировать первоначальный список задач
 renderTodos();
